ad9ab7ebdb4f20cfacf64c716e7214d1fd91d8a2
ekaone <ekaone3033@gmail.com>
2020-12-07 10:05:12 +0700

    add props button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
-import {
-  ChakraProvider,
-  CSSReset,
-  theme,
-  extendTheme,
-  Text
-} from "@chakra-ui/react";
+import { ChakraProvider, CSSReset, extendTheme, Text } from "@chakra-ui/react";
 // components
 import { Button, Breadcrumb } from "./components";
-
-const customTheme = extendTheme({
-  colors: {
-    brand: {
-      100: "#f7fafc",
-      // ...
-      900: "#FF0000"
-    }
-  }
-});
 
 function App() {
   return (
     <div>
-      <ChakraProvider theme={customTheme}>
+      <ChakraProvider>
         <CSSReset />
-        <Text color="brand.900">Chakra UI</Text>
-        <br />
-        <Breadcrumb />
-        <br />
-        <Button />
+        <Button value={"Button"} colorScheme={"yellow"} />
       </ChakraProvider>
     </div>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Button } from "@chakra-ui/react";
 
-function index() {
-  return <div>Button</div>;
+function index(props) {
+  const { value } = props;
+  return <Button {...props}>{value}</Button>;
 }
 
 export default index;
