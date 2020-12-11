b4a1c2176a52622addc0d112c58a52c65a79bb48
ekaone <ekaone3033@gmail.com>
2020-12-11 11:34:52 +0700

    feat: add layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { ChakraProvider, CSSReset, extendTheme, Text } from "@chakra-ui/react";
 // components
 import { Button, Breadcrumb } from "./components";
+import { theme } from "./theme";
 
 function App() {
+  // const customTheme = extendTheme(theme);
+
+  const customTheme = extendTheme(theme);
+
   return (
     <div>
-      <ChakraProvider>
+      <ChakraProvider theme={customTheme}>
         <CSSReset />
         <Button value={"Button"} colorScheme={"yellow"} />
+        <br />
+        TEst
       </ChakraProvider>
     </div>
   );
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+// export const theme = {
+//   colors: {
+//     brand: {
+//       100: "#f7fafc",
+//       // ...
+//       900: "#1a202c"
+//     }
+//   }
+// };
+
+// export const theme = {
+//     fonts: {
+//       body: ""
+//     }
+//   };
